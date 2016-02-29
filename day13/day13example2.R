308d9f2a1165d7c7e7577c3a0675b698c8efe6a1
Gota Morota <morota@unl.edu>
2016-02-28 21:30:08 -0600

    update

--- day13/day13example2.R
+++ day13/day13example2.R
@@ -1,16 +1,15 @@
-# Pew et al. (2015) related: an R package for analysing pairwise relatedness from codominant molecular markers. Molecular Ecology.
+# Pew et al. (2015) related: an R package for analysing pairwise relatedness from codominant molecular markers. Molecular Ecology Resources.
 # https://frasierlab.wordpress.com/software/
 
-# Ritland (1996) 
 library(related)
-## single locus
+## single locus -  Ritland (1996) 
 mydat <- data.frame(ID=as.character(c("1", "2", "3")), SNP1a= c(1, 2, 1), SNP1b=c(1, 1, 1))
 mydat[,1] <- as.character(mydat[,1])
 mydat
 output <- coancestry(mydat , ritland=1)
 output$relatedness
 
-## two loci
+## two loci - Ritland (1996) 
 mydat <- data.frame(ID=as.character(c("1", "2", "3")), SNP1a= c(1, 2, 1), SNP1b=c(1, 1, 1), SNP2a=c(1,1,1), SNP2b=c(1,2,2))
 mydat[,1] <- as.character(mydat[,1])
 mydat
